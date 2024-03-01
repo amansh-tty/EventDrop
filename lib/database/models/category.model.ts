4b17e24d5dab4df049f34c4d1b049973d3beb922
carnel <amanshetty300@gmail.com>
2024-03-01 20:06:39 +0530

    second commit

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,30 +1,44 @@
-"use server"
+// "use server"
 
-import { CreateCategoryParams } from "@/types"
-import { handleError } from "../utils"
-import { connectToDatabase } from "../database"
-import Category from "../database/models/category.model"
+// import { CreateCategoryParams } from "@/types"
+// import { handleError } from "../utils"
+// import { connectToDatabase } from "../database"
+// import Category from "../database/models/category.model"
 
-export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
-  try {
-    await connectToDatabase();
+// export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
+//   try {
+//     await connectToDatabase();
 
-    const newCategory = await Category.create({ name: categoryName });
+//     const newCategory = await Category.create({ name: categoryName });
 
-    return JSON.parse(JSON.stringify(newCategory));
-  } catch (error) {
-    handleError(error)
-  }
+//     return JSON.parse(JSON.stringify(newCategory));
+//   } catch (error) {
+//     handleError(error)
+//   }
+// }
+
+// export const getAllCategories = async () => {
+//   try {
+//     await connectToDatabase();
+
+//     const categories = await Category.find();
+
+//     return JSON.parse(JSON.stringify(categories));
+//   } catch (error) {
+//     handleError(error)
+//   }
+// }
+
+import { Document,Schema,model,models } from "mongoose"
+
+export interface ICategory extends Document{
+    _id:string;
+    name:string;
 }
 
-export const getAllCategories = async () => {
-  try {
-    await connectToDatabase();
+const CategorySchema = new Schema({
+    name:{type:String,required:true,unique:true},
+})
+const Category = models.Category || model('Category',CategorySchema)
 
-    const categories = await Category.find();
-
-    return JSON.parse(JSON.stringify(categories));
-  } catch (error) {
-    handleError(error)
-  }
-}
+export default Category
